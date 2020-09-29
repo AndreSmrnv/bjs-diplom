57a45768ff5583c178027dd019a9b264812e1448
AndreSm <tehmir@bk.ru>
2020-09-29 14:21:58 +0300

    commit homePage removeUserFromFavorites

--- public/homePage.js
+++ public/homePage.js
@@ -5,17 +5,13 @@
 const logoutButton = new LogoutButton();
 
 logoutButton.action = () => ApiConnector.logout(response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.success);
         location.reload();
     }    
 });
 //#
 ApiConnector.current(response => {
-        console.log(response);
         if (response.success) {
-            console.log(response.success);
             ProfileWidget.showProfile(response.data);
         }
     }
@@ -24,9 +20,7 @@ ApiConnector.current(response => {
 const ratesBoard = new RatesBoard();
 function getExchangeRate () {
     ApiConnector.getStocks(response => {
-            console.log(response);
             if (response.success) {
-                console.log(response.success);
                 ratesBoard.clearTable;
                 ratesBoard.fillTable(response.data);
             }
@@ -39,76 +33,75 @@ let timerIdRefreshExchangeRate = setInterval(() => getExchangeRate (), 60000);
 //#
 const moneyManager = new MoneyManager();
 moneyManager.addMoneyCallback = data => ApiConnector.addMoney(data,response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.success);
         ProfileWidget.showProfile(response.data);
         moneyManager.setMessage(response.success, 'Балланс пополнен');
     } 
     else {
-        console.log(response);
         moneyManager.setMessage(response.success, response.error);
     }   
 });
 //#
 moneyManager.conversionMoneyCallback = data => ApiConnector.convertMoney(data,response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.success);
         ProfileWidget.showProfile(response.data);
         moneyManager.setMessage(response.success, 'Сумма сконвертирована');
     } 
     else {
-        console.log(response);
         moneyManager.setMessage(response.success, response.error);
     }   
 });
 //#
 moneyManager.sendMoneyCallback = data => ApiConnector.transferMoney(data,response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.success);
         ProfileWidget.showProfile(response.data);
         moneyManager.setMessage(response.success, 'Перевод успешно завершен');
     } 
     else {
-        console.log(response);
         moneyManager.setMessage(response.success, response.error);
     }   
 });
 //#
 const favoritesWidget = new FavoritesWidget();
 ApiConnector.getFavorites(response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.success);
         favoritesWidget.clearTable();
         favoritesWidget.fillTable(response.data);
         moneyManager.updateUsersList(response.data);
     }
     else {
-        console.log(response);
         favoritesWidget.setMessage(response.success, response.error);
     } 
 }
 );
 //#
 favoritesWidget.addUserCallback = data => ApiConnector.addUserToFavorites(data,response => {
-    console.log(response);
+    
     if (response.success) {
-        console.log(response.success);
         favoritesWidget.clearTable();
         favoritesWidget.fillTable(response.data);
         moneyManager.updateUsersList(response.data);
-        favoritesWidget.setMessage(response.success, 'Пользователь добавлен');
+        favoritesWidget.setMessage(response.success, 'Пользователь добавлен в избранное');
     } 
     else {
-        console.log(response);
         favoritesWidget.setMessage(response.success, response.error);
     }   
 });
-// addUserCallback
+//#
+favoritesWidget.removeUserCallback = data => ApiConnector.removeUserFromFavorites(data,response => {
+    
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(response.success, 'Пользователь удален из избранного');
+    } 
+    else {        
+        favoritesWidget.setMessage(response.success, response.error);
+    }   
+});
+// removeUserCallback
 
 /*
-addMoney({ currency, amount }, callback) — запрос на добавление денег авторизованному пользователю
+
 */
--- public/loginPage.js
+++ public/loginPage.js
@@ -4,25 +4,19 @@
 
 const userForm = new UserForm();
 userForm.loginFormCallback = data => ApiConnector.login(data, response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.userId);
         location.reload();
     }
     else {
-        console.log(response.error);
         userForm.setLoginErrorMessage(response.error);
     }
     
 });
 userForm.registerFormCallback = data => ApiConnector.register(data, response => {
-    console.log(response);
     if (response.success) {
-        console.log(response.userId);
         location.reload();
     }
     else {
-        console.log(response.error);
         userForm.setRegisterErrorMessage(response.error);
     }
     
